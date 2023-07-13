93053f209c5d6494831b02c35d776b148adf143e
Dima Usov <dmitry.usov@yahoo.com>
2023-07-13 23:36:13 +0300

    feat: add websocket server

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 import { httpServer } from "./src/http_server/index.js";
 import { WebSocketServer} from 'ws';
-import * as my_func from "./src/ws_server/index.ts";
+import { printHi } from "./src/ws_server/index.js";
 
 const HTTP_PORT = 8181;
+const WS_PORT = 3000;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
-my_func.printHi();
 
-// const wss = new WebSocketServer({port: 3000});
-// wss.on('connection', (ws) => {
-//   console.log("a user connected");
-//   ws.on('message', (data) => {
-//     console.log('Message from client', data);
-//   })
-// });
+console.log(`Start websocket server on the ${WS_PORT} port!`);
+const wss = new WebSocketServer({port: WS_PORT});
+printHi();
+wss.on('connection', (wsc) => {
+  console.log('a user connected');
+  wsc.on('open', () => {
+    console.log('Opened');
+  });
+  wsc.on('message', (data) => {
+    console.log('Message from client', JSON.parse(data));
+  });
+  wsc.on('close', () => {
+    console.log('Closed');
+  });
+  wsc.on('error', console.error);
+});
 
