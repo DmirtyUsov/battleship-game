79fb68f5053f54dde4b4045d1bab3ff5ea4d21e1
Dima Usov <dmitry.usov@yahoo.com>
2023-07-17 12:17:52 +0300

    refactor: add array of responses to send

--- src/ws_controller/index.ts
+++ src/ws_controller/index.ts
@@ -2,58 +2,69 @@ import { RoomsDB } from "./roomsdb.js";
 import { User, Client, WSSCmd, WSSMessage, WebSocket2, Room } from "./types.js";
 import { UsersDB } from "./usersdb.js";
 
+interface WSSResponse {
+    connection: WebSocket2,
+    message: WSSMessage
+}
+
 const clients: Client = {};
 const users = new UsersDB();
 const rooms = new RoomsDB();
 
 
-const handleCmd = (message: WSSMessage,wsc: WebSocket2): WSSMessage => {
-    let type: WSSCmd = message.type;
-    const payload = message.data ? JSON.parse(message.data) : '';
-    let data = '';
+const handleCmd = (message: WSSMessage, wsc: WebSocket2): WSSResponse[] => {
+  let type: WSSCmd = message.type;
+  const payload = message.data ? JSON.parse(message.data) : '';
+  let data = '';
 
-    switch (message.type) {
-        case WSSCmd.reg: {
-            const newUser:User = users.add(payload.name, payload.password);
-            if(!newUser.error) {
-                if(clients[newUser.index]) {
-                    newUser.error = true;
-                    newUser.errorText = 'User already connected';
-                } else {
-                    wsc.userId = newUser.index;
-                    clients[newUser.index] = wsc;
-                }
-            }
-            data = JSON.stringify(newUser);
-            break;
+  const responses: WSSResponse[] = [];
+
+  switch (message.type) {
+    case WSSCmd.reg: {
+      const newUser: User = users.add(payload.name, payload.password);
+      if (!newUser.error) {
+        if (clients[newUser.index]) {
+          newUser.error = true;
+          newUser.errorText = 'User already connected';
+        } else {
+          wsc.userId = newUser.index;
+          clients[newUser.index] = wsc;
         }
-        case WSSCmd.create_room: {
-            if(!wsc.userRoomId) { // one room per user?
-                const currentUser = users.getById(wsc.userId);
-                const newRoom = rooms.create(currentUser.index, currentUser.name);
-                wsc.userRoomId = newRoom.roomId;
-            }
-            const roomsSingle: Room[] = rooms.listRoomsSingleUser();
-            
-            type = WSSCmd.update_room;
-            data = JSON.stringify(roomsSingle); 
-            break;
-        }
-
-        case WSSCmd.add_user_to_room: {
-            if(payload.indexRoom !== wsc.userRoomId) {
-                rooms.addToRoom(payload.indexRoom, wsc.userRoomId);
-            }
-            const roomsSingle: Room[] = rooms.listRoomsSingleUser();
-
-            type = WSSCmd.update_room;
-            data = JSON.stringify(roomsSingle); 
-            break;
-
-        }
+      }
+      data = JSON.stringify(newUser);
+      responses.push({ connection: wsc, message: { type, data, id: 0 } });
+      break;
     }
-    const response: WSSMessage = {type, data, id:0}
-    return response;
+    case WSSCmd.create_room: {
+      if (!wsc.userRoomId) {
+        // one room per user?
+        const currentUser = users.getById(wsc.userId);
+        const newRoom = rooms.create(currentUser.index, currentUser.name);
+        wsc.userRoomId = newRoom.roomId;
+      }
+      const roomsSingle: Room[] = rooms.listRoomsSingleUser();
+      type = WSSCmd.update_room;
+      data = JSON.stringify(roomsSingle);
+      Object.values(clients).forEach((connection) => {
+        if (connection) {
+          responses.push({ connection, message: { type, data, id: 0 } })
+        }
+      });
+      break;
+    }
+
+    case WSSCmd.add_user_to_room: {
+      if (payload.indexRoom !== wsc.userRoomId) {
+        rooms.addToRoom(payload.indexRoom, wsc.userRoomId);
+      }
+      const roomsSingle: Room[] = rooms.listRoomsSingleUser();
+
+      type = WSSCmd.update_room;
+      data = JSON.stringify(roomsSingle);
+      break;
+    }
+  }
+  return responses;
 };
 
 export const controlWSConnection = (wsc: WebSocket2) => {
@@ -66,22 +77,14 @@ export const controlWSConnection = (wsc: WebSocket2) => {
     wsc.on('message', (data) => {
         const messageIn: WSSMessage = JSON.parse(data.toString());
         console.log('Message from client', messageIn);
-        const messageOut = handleCmd(messageIn, wsc);
-        const messateOutTxt = JSON.stringify(messageOut);
-        console.log('Message to Client', messageOut);
-        switch(messageOut.type) {
-            case WSSCmd.update_room: {
-                Object.values(clients).forEach((connestion) => {
-                    if(connestion) {
-                        connestion.send(messateOutTxt);
-                    }
-                })
-                break;
+        const responses = handleCmd(messageIn, wsc);
+        responses.forEach((response) => {
+            if(response.connection) {
+                const messageOutTxt = JSON.stringify(response.message);
+                response.connection.send(messageOutTxt);
+                console.log('Message to Client', response.message);
             }
-            default: {
-                wsc.send(messateOutTxt);
-            }
-        }
+        });
     });
 
     wsc.on('close', () => {
